2c72f00ec4ef87e9fe9dc14e0aed21de5559d24e
AlexandruPascali <pascali.alex@gmail.com>
2024-04-14 11:41:55 +0300

    Added 16 primitive variables as a practical exercise

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,43 @@
+package md.tekwillacademy.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+
+        // Declare and initialize the primitive data type variables
+
+        boolean isFriday = true;
+        System.out.println("Today is working day: " + isFriday);
+        boolean isWeekend = false;
+        System.out.println("Today is weekend: " + isWeekend);
+        char exclamMark = '!';
+        System.out.println("Exclamation mark is: " + exclamMark);
+        char dollarSign = '$';
+        System.out.println("Dollar sign is: " + dollarSign);
+        byte minByteValues = -128;
+        System.out.println("Minimal option for byte is: " + minByteValues);
+        byte maxByteValues = 127;
+        System.out.println("Maximal option for byte is: " + maxByteValues);
+        short minShortValues = -32768;
+        System.out.println("Minimal option for short is: " + minShortValues);
+        short maxShortValues = 32767;
+        System.out.println("Maximal option for short is: " + maxShortValues);
+        int minCarSpeed = 40;
+        System.out.println("Minimal car speed is: " + minCarSpeed);
+        int maxCarSpeed = 120;
+        System.out.println("Maximal car speed is: " + maxCarSpeed);
+        long kmFromEarthToMars = 225000000;
+        System.out.println("The distance from Earth to Mars is: " + kmFromEarthToMars);
+        long kmFromEarthToPluto = 500000000;
+        System.out.println("The distance from Earth to Pluto is: " + kmFromEarthToPluto);
+        float piNumber = 3.1415f;
+        System.out.println("The Pi number is: " + piNumber);
+        float divisionResult = 10.58f;
+        System.out.println("The division result is: " + divisionResult);
+        double piNumberLong = 3.14159265358979d;
+        System.out.println("The Pi number is: " + piNumberLong);
+        double divisionResultLong = 10.583748596046395d;
+        System.out.println("The division result is: " + divisionResultLong);
+    }
+
+
+}

fe8b0b2e44be2457af30467bfd83e0f3e4d75a9f
AlexandruPascali <pascali.alex@gmail.com>
2024-05-14 20:15:20 +0300

    Added the Java tasks class practice

---
+++ src/main/java/md/tekwillacademy/classpractice/PracticeExecution.java
@@ -0,0 +1,12 @@
+package md.tekwillacademy.classpractice;
+
+public class PracticeExecution {
+    public static void main(String[] args) {
+
+        System.out.println(PalindromeAndReverse.reverseString("mAnuFactuRa"));
+        System.out.println(PalindromeAndReverse.isPalindrome("ABBA"));
+        PrintNumberTillTheEnd.printNumberTillTheEnd(20);
+    }
+
+
+}
---
+++ src/main/java/md/tekwillacademy/classpractice/PrintNumberTillTheEnd.java
@@ -0,0 +1,11 @@
+package md.tekwillacademy.classpractice;
+
+public class PrintNumberTillTheEnd {
+
+
+    public static void printNumberTillTheEnd(int number) {
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i);
+        }
+    }
+}
